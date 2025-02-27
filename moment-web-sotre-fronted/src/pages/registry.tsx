e349b237565225e37912c9839f2a2a2e156e3c21
moment0012 <2970332074@qq.com>
2025-02-27 21:06:26 +0800

    2025/02/22 21:06 初始化项目-后端

--- moment-web-sotre-fronted/src/pages/registry.tsx
+++ moment-web-sotre-fronted/src/pages/registry.tsx
@@ -1,9 +1,24 @@
-import {
-    LoginForm
-} from '@ant-design/pro-components'
+import { Navigate, useNavigate } from "react-router-dom";
+import { useRef } from "react";
+import { LoginForm, ProFormInstance } from '@ant-design/pro-components';
+import React from "react";
 
 const Registry = () => {
+    const navigate = useNavigate();
+    const formRef = useRef<ProFormInstance>(null);
+
+    const backToLogin = () => {
+        navigate('/login');
+    }
+
+
+    return (
+        <div style={{ backgroundColor: 'white', height: '100vh' }}>
+            <LoginForm
+
+            ></LoginForm>
+        </div>
+    )
+
 
 }
-
-export default Registry;
